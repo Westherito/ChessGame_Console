4355026d676d7a42fc8be336452cc6764db16775
Westherito <raphaelxs100@gmail.com>
2023-01-12 23:11:16 -0300

    Criada o namespace tabuleiro e classe posicao

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿
-namespace ChessGame_console
+﻿using Tabuleiro;
+using System;
+namespace chess_game_console
 {
     class Program
     {
         private static void Main(string[] args)
         {
-
+            Posicao P = new Posicao(3,4);
+            Console.WriteLine(P);
+            
         }
+
+        
     }
 }
